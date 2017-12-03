336eee8dd93479c0fafe195a04980827cbe7fd59
xinhe huang <huntriver@hotmail.com>
2017-12-03 12:26:34 +0800

    add deploy script

---
+++ scripts/make_and_deploy_to_test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+SCRIPT_PATH=$(dirname $0)
+cd $SCRIPT_PATH/..
+mvn clean install
+sudo mkdir -p /opt/job-admin
+sudo mkdir -p /opt/job-executor
+sudo cp job-admin/target/job-admin-1.0-SNAPSHOT.jar /opt/job-admin
+sudo cp job-executor/target/job-executor-1.0-SNAPSHOT.jar /opt/job-executor
+sudo ln -s /opt/job-admin/job-admin-1.0-SNAPSHOT.jar /etc/init.d/job-admin
+sudo ln -s /opt/job-executor/job-executor-1.0-SNAPSHOT.jar /etc/init.d/job-executor
+sudo /etc/init.d/job-admin restart
+sudo /etc/init.d/job-executor restart
